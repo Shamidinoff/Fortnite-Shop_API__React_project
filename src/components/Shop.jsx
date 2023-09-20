98269f2d3a809c8df494d2e2dad4884e3e6abdf9
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-09-20 23:45:38 +0600

    Fortnite Shop - React project

---
+++ src/components/Shop.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react"
+import { API_KEY, API_URL } from "../config";
+
+
+import { Preloader } from "./Preloader";
+import { GoodsList } from "./GoodsList";
+
+function Shop() {
+    const [goods, setGoods] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(function getGoods() {
+        fetch(API_URL, {
+            headers: {
+                "Authorization": API_KEY,
+            }
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                data.featured && setGoods(data.featured); // featured
+                setLoading(false);
+            })
+    }, [])
+
+    return <main className="container content">
+        {
+            loading ? <Preloader /> : <GoodsList goods={goods} />
+        }
+    </main>
+}
+
+export { Shop }
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const API_KEY = "b0702198-510c9c72-80dcaecd-06d15e99";
+
+const API_URL = "https://fortniteapi.io/v2/shop?lang=ru";
+
+export { API_KEY, API_URL };
