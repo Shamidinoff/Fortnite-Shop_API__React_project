f218ef99ea83aff7c3d23c1c84dd1dd62664a70c
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-09-23 17:47:27 +0600

    Fortnite Shop - React project

--- src/components/BasketItem.jsx
+++ src/components/BasketItem.jsx
@@ -1,7 +1,11 @@
 function BasketItem(props) {
-    const { id, name, price, quantity, removeFromBasket = Function.prototype } = props;
+    const { id, name, price, quantity,
+        removeFromBasket = Function.prototype,
+        decQuantity = Function.prototype,
+        incQuantity = Function.prototype,
+    } = props;
     return <li className="collection-item ">
-        {name} x {quantity} = {price * quantity} тенге
+        {name}  <i className="material-icons basket-quantity" onClick={() => decQuantity}>remove</i> x{quantity}{' '} <i className="material-icons basket-quantity" onClick={() => incQuantity}>add</i> = {price * quantity} тенге
         <span class="secondary-content" onClick={() => removeFromBasket(id)}>
             <i class="material-icons basket-delete">close</i>
         </span>
